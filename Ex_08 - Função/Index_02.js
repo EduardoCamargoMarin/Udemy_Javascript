ef8c45a55b0af87a6e8d604e309bd0b3920819bc
Eduardo <du.marin10@gmail.com>
2022-12-28 17:41:48 -0300

    Udemy_Aulas_Funcões

---
+++ Ex_08 - Função/Index_02.js
@@ -0,0 +1,2 @@
+let price = 30
+console.log(`transaction approved! $${price},00`)
---
+++ Ex_08 - Função/index.js
@@ -0,0 +1,6 @@
+
+function  saleStatus(status, total) {
+    console.log('Transaction ' + status + '! Total amount: $' + total)
+}
+
+saleStatus('Approved', 30)
