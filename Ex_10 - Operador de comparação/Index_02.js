998d89e22d89c9e6c19640032aee6f9e586650ab
Eduardo <du.marin10@gmail.com>
2022-12-29 12:42:56 -0300

    Udemy_Aulas_Op. de Comparação

---
+++ Ex_10 - Operador de comparação/Index_02.js
@@ -0,0 +1,14 @@
+
+let num1 = 3
+
+
+console.log(num1 > 9)
+console.log(num1 <1)
+console.log(num1 >= 0)
+console.log(num1 <= 5)
+
+
+//COMPARAÇÃO
+//console.error(num1 !== 3) não é igual
+//console.error(num1 === 3)  é igual
+
