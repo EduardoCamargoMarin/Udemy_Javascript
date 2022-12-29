031d8ab52728d66d88fc43335a66b7a0e2bf7e4e
Eduardo <du.marin10@gmail.com>
2022-12-29 09:31:18 -0300

    Udemy_Aulas_Operador Aritimetico

---
+++ Ex_09 - Operador aritimetico/Index_02.js
@@ -0,0 +1,16 @@
+
+let num1 = 3
+let num2 = 2
+
+let total = num1 + num2
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2) // Resto da divisão
+console.log(num1 ** num2) // Exponencial
+
+console.log(++total) // incrementar em +1 = 6
+console.log(--total) // decremento de -1 = 5
+console.log(--total) // decremento de -1 = 4
