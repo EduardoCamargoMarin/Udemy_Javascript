8fce5a16628f7d4ec908d7d5b669b234cf79702d
Eduardo <du.marin10@gmail.com>
2022-12-29 10:16:33 -0300

    Udemy_Aulas_Op. de Atribuição

---
+++ Ex_09 - Operador de atribuição/Index_02.js
@@ -0,0 +1,11 @@
+
+let num1 = 3
+
+//num1 = num1 + 20
+
+//num1 += 20 //Operador de atribuição
+//num1 -= 20 //Operador de atribuição
+//num1 *= 20 //Operador de atribuição
+//num1 /= 20 //Operador de atribuição
+
+console.log(num1)
