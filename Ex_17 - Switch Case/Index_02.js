382a931bfbceb1d2ce222e2c184ce249489cd9f6
Eduardo <du.marin10@gmail.com>
2022-12-29 16:46:26 -0300

    Udemy_Aulas_Switch/Case

---
+++ Ex_17 - Switch Case/Index_02.js
@@ -0,0 +1,15 @@
+let airport = 'JFK '
+
+
+
+switch (airport) {
+    case 'MCO':
+        console.log('Orlando')
+        break
+    case 'JFK':
+        console.log('John F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Seattle')
+        break
+}
