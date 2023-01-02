ce4b5e8e6c72982f0130121391e55963b7a1fff2
Eduardo <du.marin10@gmail.com>
2023-01-02 17:27:44 -0300

    Udemy_Aulas_Template Literals

---
+++ Ex_30 - String/Index_02.js
@@ -0,0 +1,8 @@
+
+
+let message = 'Hey, my name is Eduardo'
+
+console.log(message.startsWith('Hey'))
+console.log(message.length)
+console.log(message.includes('eduardo'))
+console.log(message.endsWith('do'))
---
+++ Ex_31 - Template Literals/Index_02.js
@@ -0,0 +1,13 @@
+
+
+// const email = ' Hi Joe, \n the meeting is confirmed!\n Eduardo' - não é ideal para codigos
+
+
+
+let firstName ='Joe' //Interpolação
+//funciona se trocar pela crase
+
+const email = `Hi ${firstName}, 
+The meeting is confirmed! 
+Eduardo`
+console.log(email)
