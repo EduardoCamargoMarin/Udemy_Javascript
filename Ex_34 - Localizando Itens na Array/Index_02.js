8bce8b832962ad9e01dc47944a17447ad3ff2e69
Eduardo <du.marin10@gmail.com>
2023-01-03 10:56:28 -0300

    Udemy_Aulas_Localizando item na Array

---
+++ Ex_34 - Localizando Itens na Array/Index_02.js
@@ -0,0 +1,9 @@
+
+
+const num = [6, 7, 8, 9]
+
+console.log(num.indexOf(8)) // apresenta a posição
+
+console.log(num.indexOf(4)) // apresenta a posição -1 (indicando que não tem)
+
+console.log(num.includes(8)) // mostra se o valor existe na array
