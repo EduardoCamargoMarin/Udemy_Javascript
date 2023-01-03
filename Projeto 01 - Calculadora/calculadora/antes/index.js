70532928bd47b7e26628350b6dce6158cb6dd101
Eduardo <du.marin10@gmail.com>
2023-01-03 17:47:18 -0300

    Udemy_Aulas_Projeto Calculadora

--- Projeto 01 - Calculadora/calculadora/antes/index.js
+++ Projeto 01 - Calculadora/calculadora/antes/index.js
@@ -1,12 +1,10 @@
-
-
 function outcome() {
 
-    let num1 = Number(document.getElementById('num-one').value)
+    let num1 = Number(document.getElementById('num-one').value) // Number o valor ser tratado como numero, chama pelo id, value para aceitar qualquer valor
     let num2 = Number(document.getElementById('num-two').value)
-    let total = 0
+    let total = 0 // sempre o valor total ser igual a zero
 
-    if (document.getElementById('box1').checked)
+    if (document.getElementById('box1').checked) // condição para se a opção da operação for selecionada.
         total = num1 + num2
     else if (document.getElementById('box2').checked)
         total = num1 - num2
@@ -14,5 +12,5 @@ function outcome() {
         total = num1 * num2
     else
         total = num1 / num2
-    document.getElementById('resultArea').innerHTML = 'Result: ' + String(total)
+    document.getElementById('resultArea').innerHTML = 'Result: ' + String(total) //apresenta o resultado calculado como o valor de string.
 }
