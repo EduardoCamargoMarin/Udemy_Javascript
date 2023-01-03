6441362a85d236ea2d8f8112df992ee71135ca1c
Eduardo <du.marin10@gmail.com>
2023-01-03 10:52:53 -0300

    Udemy_Aulas_Valores na Array

--- Ex_33 - Adicionando Valores Array/Index_02.js
+++ Ex_33 - Adicionando Valores Array/Index_02.js
@@ -1,7 +1,11 @@
 
 
-let friends = ['Marcos', 'Maria', 'Bia', 'Joe', 'Peter']
-friends.push('Amanda') // adiciona
-friends[2] = 'Julia' // substitui
-console.log(friends)
+const num = [7, 8, 9]
 
+num.push(11, 12, 13, 14, 15, 16, 17, 18, 19, 20) //adiciona no final da array
+
+num.unshift(0, 20, 30, 40) // adiciona no começo da array
+
+num.splice(3, 0, 100, 200, 300, 400) // adiciona na posição que você definir na array
+
+console.log(num)
