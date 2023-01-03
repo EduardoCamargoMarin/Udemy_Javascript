872bb3c91b1278536fb46e9f9afc260c75520c37
Eduardo <du.marin10@gmail.com>
2023-01-03 10:46:22 -0300

    Udemy_Aulas_Arrays

--- DESAFIO 02/index.js
+++ DESAFIO 02/index.js
@@ -1,6 +1,7 @@
 //MarketPlace onde não posso vender produtos com valor abaixo de $20,00
 
-let productValue = Number(prompt('Enter the product value: ')) //o tipo vira numero se tiver o Number
+let productValue = Number(prompt('Enter the product value: ')) 
+//o tipo vira numero se tiver o Number
 
 if (productValue >= 20) {
 document.getElementById('result').innerHTML = 'Approved'
---
+++ Ex_32 - Arrays/Index_02.js
@@ -0,0 +1,7 @@
+
+
+let friends = ['Marcos', 'Maria', 'Bia', 'Joe', 'Peter']
+friends.push('Amanda') // adiciona
+friends[2] = 'Julia' // substitui
+console.log(friends)
+
