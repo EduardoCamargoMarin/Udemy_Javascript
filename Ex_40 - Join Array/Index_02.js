a6809ff8d6bd66d1ae578bd3aeb314335fb9ac19
Eduardo <du.marin10@gmail.com>
2023-01-03 13:27:03 -0300

    Udemy_Aulas_Arrays

--- Ex_40 - Join Array/Index_02.js
+++ Ex_40 - Join Array/Index_02.js
@@ -1,13 +1,8 @@
 
 
+let clients = ['Andre', 'José', 'Marcela']
+console.log(clients)
 
-let numbers = [5, 6, 7, 8]
-let letters = ['a', 'b', 'c']
+let clientsJoin = clients.join(', ') //serve para adicionar qualquer coisa na array
 
-all = numbers.concat(letters) //concatenação de arrays
-console.log(all)
-
-//pegar uma porção especifica e fazer uma nova array
-half = all.slice(2, 6) // O comando slice pega a porça que você deseja.
-console.log(half)
-
+console.log(clientsJoin)
---
+++ Ex_42 -  Verificando elementos na array/Index_02.js
@@ -0,0 +1,9 @@
+
+
+let clients = ['Andre', 'Marcela', 'José']
+
+// clients.sort() -- organizar de forma alfabetica e numerica
+
+clients.reverse() // reverte a array
+
+console.log(clients)
